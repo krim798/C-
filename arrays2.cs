5c320b43371c44a4ee852a429447c08c4bf34375
krishan798 <122988666+krim798@users.noreply.github.com>
2023-11-10 23:32:06 +0530

    Create arrays2.cs

---
+++ arrays2.cs
@@ -0,0 +1,19 @@
+// The company needs you to complete an inventory of your warehouse to determine the number of products that are ready to ship. In addition to the total number of finished products, you need to report the number of finished products stored in each individual bin in your warehouse, along with a running total. This running total will be used to create an audit trail so you can double-check your work and identify "shrinkage".
+string[] names = {"Rowena","Robin","Bao"};
+foreach(string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int[] inventory = {200, 450, 750, 175, 200};
+int sum=0;
+int bin = 0;
+
+foreach(int items in inventory)
+{
+    sum+=items;     
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total:{sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory.");
+
